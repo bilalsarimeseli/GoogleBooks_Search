b24e71d5f37057a824ca0b7f07d3757e2957d9b5
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-11-09 00:17:10 -0800

    routes added

--- server.js
+++ server.js
@@ -8,3 +8,22 @@ const routes = require("./routes")
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+//For connecting MongoDB
+const mongoose = require("mongoose");
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/googlebookssearch");
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// Add routes, both API and view
+app.use(routes);
+
+const PORT = process.env.PORT || 3001;
+// Start the API server
+app.listen(PORT, function() {
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+});

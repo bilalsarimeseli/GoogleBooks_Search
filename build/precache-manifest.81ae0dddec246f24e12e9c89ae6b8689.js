6a6247b1a6dbdb229c4ff4121e81f956af8f2177
Bilal Sarımeşeli <bilalsarimeseli@Bilals-Air.attlocal.net>
2019-11-09 20:57:09 -0800

    build

---
+++ build/precache-manifest.81ae0dddec246f24e12e9c89ae6b8689.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "681259f7c10e0b7af0712cd3968f9c6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c87ec7a5c6471ac490c",
+    "url": "/static/css/2.53c20c32.chunk.css"
+  },
+  {
+    "revision": "a539c17ece7a751f7d14",
+    "url": "/static/css/main.bff32a20.chunk.css"
+  },
+  {
+    "revision": "3c87ec7a5c6471ac490c",
+    "url": "/static/js/2.c06acf9b.chunk.js"
+  },
+  {
+    "revision": "a539c17ece7a751f7d14",
+    "url": "/static/js/main.e5197831.chunk.js"
+  },
+  {
+    "revision": "b4efb973f9e5c3b534e6",
+    "url": "/static/js/runtime-main.76dad532.js"
+  }
+]);
